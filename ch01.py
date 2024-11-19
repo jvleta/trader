c3e1504992bd28d2a29e54601321ffa87e99ed8f
Jimmy Leta <jimmy.leta@gmail.com>
2024-11-18 21:03:23 -0500

    Implemented calculations for other payouts and moved them to separate file

--- ch01.py
+++ ch01.py
@@ -1,18 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from payout import compute_bull_spread_payout, compute_call_option_payout, compute_put_option_payout, compute_butterfly_spread_payout
 
 
-def make_payout_diagram(E1, E2):
+def make_call_option_payout_diagram():
     S = np.linspace(0, 6, 100)
-    B = np.array([max(s - E1, 0) - max(s - E2, 0) for s in S])
-    plt.plot(S, B)
-    plt.ylabel('S')
-    plt.xlabel('B')
-    plt.show()
+    E = 2
+    C = compute_call_option_payout(S, E)
+
+    plt.figure()
+    plt.plot(S, C)
+    plt.xlabel(r'$S(T)$')
+    plt.ylabel(r'$C$')
+    plt.title('Call Option Payout')
     plt.grid()
 
-if __name__ == "__main__":
+
+def make_put_option_payout_diagram():
+    S = np.linspace(0, 6, 100)
+    E = 2
+    P = compute_put_option_payout(S, E)
+
+    plt.figure()
+    plt.plot(S, P)
+    plt.xlabel(r'$S(T)$')
+    plt.ylabel(r'$P$')
+    plt.title('Put Option Payout')
+    plt.grid()
+
+
+def make_bull_spread_payout_diagram():
+    S = np.linspace(0, 6, 100)
     E1 = 2
     E2 = 4
+    B = compute_bull_spread_payout(S, E1, E2)
 
-    make_payout_diagram(E1, E2)
+    plt.figure()
+    plt.plot(S, B)
+    plt.xlabel(r'$S(T)$')
+    plt.ylabel(r'$B$')
+    plt.title('Bull Spread Payout')
+    plt.grid()
+
+
+def make_butterfly_spread_payout_diagram():
+    S = np.linspace(0, 6, 1000)
+    E1 = 2
+    E2 = 3
+    E3 = 4
+
+    B = compute_butterfly_spread_payout(S, E1, E2, E3)
+
+    plt.figure()
+    plt.plot(S, B)
+    plt.xlabel(r'$S(T)$')
+    plt.ylabel(r'$B$')
+    plt.title('Butterfly Spread Payout')
+    plt.grid()
+
+
+if __name__ == "__main__":
+    make_call_option_payout_diagram()
+    make_put_option_payout_diagram()
+    make_bull_spread_payout_diagram()
+    make_butterfly_spread_payout_diagram()
+
+    plt.show()
