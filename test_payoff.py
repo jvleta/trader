48f836f0c9997edc2e7ecd1ccd950034c608d9cc
Jimmy Leta <jimmy.leta@gmail.com>
2025-11-28 13:26:49 -0500

    first draft of unit tests

---
+++ test_payoff.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import payoff
+
+matplotlib.use("Agg")
+
+
+def test_payoff_functions():
+    asset_prices = np.array([80, 90, 100, 110, 120], dtype=float)
+
+    np.testing.assert_array_equal(
+        payoff.long_call_payoff(asset_prices, strike=100),
+        np.array([0, 0, 0, 10, 20], dtype=float),
+    )
+    np.testing.assert_array_equal(
+        payoff.long_put_payoff(asset_prices, strike=100),
+        np.array([20, 10, 0, 0, 0], dtype=float),
+    )
+    np.testing.assert_array_equal(
+        payoff.short_call_payoff(asset_prices, strike=100),
+        -payoff.long_call_payoff(asset_prices, 100),
+    )
+    np.testing.assert_array_equal(
+        payoff.short_put_payoff(asset_prices, strike=100),
+        -payoff.long_put_payoff(asset_prices, 100),
+    )
+
+    np.testing.assert_array_equal(
+        payoff.bull_spread_payoff(asset_prices, lower_strike=90, upper_strike=110),
+        np.array([0, 0, 10, 20, 20], dtype=float),
+    )
+    np.testing.assert_array_equal(
+        payoff.butterfly_spread_payoff(
+            asset_prices, low_strike=90, mid_strike=100, high_strike=110
+        ),
+        np.array([0, 0, 10, 0, 0], dtype=float),
+    )
+    np.testing.assert_array_equal(
+        payoff.butterfly_spread_payoff(
+            np.array([105], dtype=float), low_strike=90, mid_strike=100, high_strike=110
+        ),
+        np.array([5], dtype=float),
+    )
+
+    call_payoff = payoff.make_payoff(payoff.long_call_payoff, strike=100)
+    np.testing.assert_array_equal(
+        call_payoff(asset_prices),
+        payoff.long_call_payoff(asset_prices, 100),
+    )
+
+
+def test_plot_payoff(monkeypatch):
+    shown = {"called": False}
+    monkeypatch.setattr(plt, "show", lambda: shown.__setitem__("called", True))
+    plt.close("all")
+
+    call_payoff = payoff.make_payoff(payoff.long_call_payoff, strike=100)
+    payoff.plot_payoff(call_payoff, min_asset_price=80, max_asset_price=120)
+
+    fig = plt.gcf()
+    ax = fig.axes[0]
+    line = ax.lines[0]
+
+    xdata = np.asarray(line.get_xdata(), dtype=float)
+    ydata = np.asarray(line.get_ydata(), dtype=float)
+
+    assert shown["called"]
+    assert len(xdata) == 1000
+    np.testing.assert_allclose(ydata, payoff.long_call_payoff(xdata, strike=100))
